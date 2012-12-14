51d3be0387f8766c0ffa108e186208214469a160
James Gill <jamesagill@gmail.com>
2012-12-13 17:55:58 -0800

    Integration build checks out the correct branch.

--- jenkins/integrationBuild.sh
+++ jenkins/integrationBuild.sh
@@ -1,7 +1,7 @@
 printenv
-[ -n "$modifiedBranch" ] && git checkout $modifiedBranch
-[ -n "$modifiedBranch" ] && git submodule foreach "git checkout $GIT_BRANCH"
+git checkout $GIT_BRANCH
+git submodule foreach "git checkout $GIT_BRANCH"
 ls -la
 ./deploy.rb --ec2 --branch=$GIT_BRANCH
 #RUN TESTS HERE
-[ -n "$modifiedBranch" ] && git commit -a -m "Updating submodule commits.  Automated by Jenkins." || echo "No changes found."
+git commit -a -m "Updating submodule commits.  Automated by Jenkins." || echo "No changes found."
