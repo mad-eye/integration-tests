97e2e582ddc7b004e721c1e1028b640de16444bf
James Gill <jamesagill@gmail.com>
2012-12-20 09:08:53 -0800

    Updating submodule commits.  Automated by Jenkins.

--- jenkins/scripts/integration.sh
+++ jenkins/scripts/integration.sh
@@ -1,7 +1,9 @@
 printenv
 git checkout $GIT_BRANCH
-git submodule foreach "git checkout $GIT_BRANCH"
+./updateSubmodules.sh
 ls -la
 ./deploy.rb --ec2 --branch=$GIT_BRANCH
 #RUN TESTS HERE
 git commit -a -m "Updating submodule commits.  Automated by Jenkins." || echo "No changes found."
+git push
+
