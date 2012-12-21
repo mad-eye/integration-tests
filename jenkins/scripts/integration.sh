52d0334df3480bce44185a4cf0e39fb102001e45
rissem <rissem@gmail.com>
2012-12-21 14:29:43 -0800

    add a step to pull the .madeye-common submodules .madeye-common

--- jenkins/scripts/integration.sh
+++ jenkins/scripts/integration.sh
@@ -1,6 +1,7 @@
-printenv
 git checkout $GIT_BRANCH
-./updateSubmodules.sh
+# TODO use this script? ./updateSubmodules.sh
+cd $WORKSPACE/azkaban && git submodule update --init .madeye-common
+cd $WORKSPACE/apogee && git submodule update --init .madeye-common
 ./deploy.rb --ec2 --branch=$GIT_BRANCH
 #RUN TESTS HERE
 git commit -a -m "Updating submodule commits.  Automated by Jenkins." || echo "No changes found."
