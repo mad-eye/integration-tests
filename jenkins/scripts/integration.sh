c7ebbc9477f2385060b46f9484e4c797be5818f4
rissem <rissem@gmail.com>
2012-12-21 14:46:47 -0800

    fix up some path issues

--- jenkins/scripts/integration.sh
+++ jenkins/scripts/integration.sh
@@ -2,8 +2,7 @@ git checkout $GIT_BRANCH
 # TODO use this script? ./updateSubmodules.sh
 cd $WORKSPACE/azkaban && git submodule update --init .madeye-common
 cd $WORKSPACE/apogee && git submodule update --init .madeye-common
-./deploy.rb --ec2 --branch=$GIT_BRANCH
+cd $WORKSPACE && ./deploy.rb --ec2 --branch=$GIT_BRANCH
 #RUN TESTS HERE
 git commit -a -m "Updating submodule commits.  Automated by Jenkins." || echo "No changes found."
 git push
-
