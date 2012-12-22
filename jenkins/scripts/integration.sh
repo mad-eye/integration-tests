afccdd40891cb8ead280300a33af68878e5c35b9
James Gill <jamesagill@gmail.com>
2012-12-22 11:31:05 -0500

    Updating submodule commits.  Automated by Jenkins.

--- jenkins/scripts/integration.sh
+++ jenkins/scripts/integration.sh
@@ -1,7 +1,7 @@
 git checkout $GIT_BRANCH
 sh $WORKSPACE/bin/init.sh
 for dir in apogee, azkaban, bolide, dementor; do
-    cd $WORKSPACE/dir && git checkout $GIT_BRANCH && git pull && cd $WORKSPACE
+    cd $WORKSPACE/$dir && git checkout $GIT_BRANCH && git pull && cd $WORKSPACE
 done
 cd $WORKSPACE && ./deploy.rb --ec2 --branch=$GIT_BRANCH
 #RUN TESTS HERE
