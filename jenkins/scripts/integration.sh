d6cb0ca54dbca9c40c8df5be321a43e2150bba60
James Gill <jamesagill@gmail.com>
2012-12-22 11:30:08 -0500

    Updating submodule commits.  Automated by Jenkins.

--- jenkins/scripts/integration.sh
+++ jenkins/scripts/integration.sh
@@ -1,7 +1,8 @@
 git checkout $GIT_BRANCH
-# TODO use this script? ./updateSubmodules.sh
-cd $WORKSPACE/azkaban && git submodule update --init .madeye-common
-cd $WORKSPACE/apogee && git submodule update --init .madeye-common
+sh $WORKSPACE/bin/init.sh
+for dir in apogee, azkaban, bolide, dementor; do
+    cd $WORKSPACE/dir && git checkout $GIT_BRANCH && git pull && cd $WORKSPACE
+done
 cd $WORKSPACE && ./deploy.rb --ec2 --branch=$GIT_BRANCH
 #RUN TESTS HERE
 git commit -a -m "Updating submodule commits.  Automated by Jenkins." || echo "No changes found."
