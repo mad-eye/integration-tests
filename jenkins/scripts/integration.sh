0ab8aa7723bf1ce46dbe193ab277254224070985
James Gill <jamesagill@gmail.com>
2013-01-29 23:22:23 -0800

    Adding loggly key for azkaban to jenkins configuration.

--- jenkins/scripts/integration.sh
+++ jenkins/scripts/integration.sh
@@ -23,6 +23,7 @@ export MADEYE_HTTP_PORT=4004
 export MADEYE_APOGEE_HOST=staging.madeye.io
 export MADEYE_APOGEE_PORT=3000
 export MADEYE_HOME=$WORKSPACE
+export MADEYE_LOGGLY_AZKABAN_ID=cb563a0e-fd2d-4340-85bf-ae2a2e811cc7
 
 sleep 10
 coffee tests/testRunner.coffee
