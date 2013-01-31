75409e1004a2a441007cb62b309e65e23c251c09
James Gill <jamesagill@gmail.com>
2013-01-30 16:19:06 -0800

    Correcting mongo port of azkaban script.

--- jenkins/scripts/azkaban.sh
+++ jenkins/scripts/azkaban.sh
@@ -1,14 +1,15 @@
-export MADEYE_BC_HOST=localhost
-export MADEYE_BC_PORT=4321
-export MADEYE_HTTP_HOST=localhost
-export MADEYE_HTTP_PORT=4004
-export MADEYE_MONGO_HOST=localhost
-export MADEYE_MONGO_PORT=27017
-export MADEYE_APOGEE_HOST=localhost
 export MADEYE_APOGEE_PORT=3000
-export MADEYE_BOLIDE_HOST=localhost
+export MADEYE_APOGEE_URL=http://localhost:3000
+export MADEYE_AZKABAN_PORT=4004
+export MADEYE_AZKABAN_HOST=localhost:4004
+export MADEYE_AZKABAN_URL=http://localhost:4004
 export MADEYE_BOLIDE_PORT=3003
-export MADEYE_LOGGLY_AZKABAN_KEY=cb563a0e-fd2d-4340-85bf-ae2a2e811cc7
+export MADEYE_BOLIDE_URL=http://localhost:3003/channel
+export MADEYE_MONGO_PORT=27017
+export MADEYE_MONGO_HOST=localhost
+export MADEYE_MONGO_URL=mongodb://localhost:27017/meteor
+export MADEYE_KISS_METRICS_ID=9b307f5887365b1ecb4b47eb7625c69570d05b89
+export MADEYE_LOGGLY_AZKABAN_KEY=f924ff21-2026-49e7-ab43-4a079d7d1561
 
 bin/init
 bin/run_tests
