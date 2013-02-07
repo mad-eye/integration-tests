a89c9ce4ec8d92d008bb5c3997b0f97fdb060666
James Gill <jamesagill@gmail.com>
2013-02-06 18:43:22 -0800

    Streamlined merging script.

--- bin/mergeToMaster.sh
+++ bin/mergeToMaster.sh
@@ -1,14 +1,17 @@
 #! /bin/sh
+set -ex
 
+git fetch
 git checkout develop
-git pull
+git merge origin/develop
 for dir in apogee azkaban bolide dementor madeye-common; do
     #TODO: Bump dementor version -- But how much?
     cd $dir
+    git fetch
     git checkout master
-    git pull
+    git merge origin/master
     git checkout develop
-    git pull
+    git merge origin/develop
     git merge master
     echo "Press return to continue, or cancel if this wasn't a no-op"
     read dummy_var
@@ -19,7 +22,7 @@ for dir in apogee azkaban bolide dementor madeye-common; do
 done
 
 git checkout master
-git pull
+git merge origin/master
 git merge --no-ff --no-commit develop
 bin/init
 
