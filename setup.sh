d50b435894a455581f2a2d9bcfc16308189f37b6
rissem <rissem@gmail.com>
2013-09-09 10:39:06 -0700

    first stab at mini-tywin

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+#could just include in the tarball as well..
+wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
+
+
+sudo dpkg -i puppetlabs-release-precise.deb
+sudo apt-get update -qqy
+
+sudo apt-get install -y curl
+sudo apt-get install -y puppet
+
+sudo puppet module install puppetlabs/ntp
+sudo puppet module install puppetlabs-apt
+sudo puppet module install puppetlabs-mongodb
+sudo puppet module install rtyler/jenkins
+
+sudo rsync -ruv /vagrant/madeye-ops/puppet/modules /usr/share/puppet
+
+sudo puppet apply -e "class {'appserver': environment => 'production'}"
